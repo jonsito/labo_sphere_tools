4aeccb24864a1887fe61130a2b63a2aa83c07672
jantonio <jonsito@gmail.com>
2020-08-27 15:31:19 +0200

    add server resolution code in client (first try)

--- im_alive.h
+++ im_alive.h
@@ -4,8 +4,10 @@
 #define SERVER_PORT 8877
 #ifdef DEBUG
 #define SERVER_HOST "localhost"
+#define MOUNT_DEV "/dev/md127"
 #else
 #define SERVER_HOST "acceso.lab.dit.upm.es"
+#define MOUNT_DEV "/dev/nbd0"
 #endif
 
 #define BUFFER_LENGTH 1024
--- im_alive_client.c
+++ im_alive_client.c
@@ -6,8 +6,25 @@
 #include <unistd.h>
 #include<sys/utsname.h>
 #include<utmpx.h>
+#include <mntent.h>
 
 #include "im_alive.h"
+
+/* from https://stackoverflow.com/questions/9280759/linux-function-to-get-mount-points */
+char *getServer() {
+struct mntent *ent;
+  char *result="-";
+  FILE *aFile = setmntent("/proc/mounts", "r");
+  if (aFile == NULL) {
+    perror("setmntent");
+    return result;
+  }
+  while (NULL != (ent = getmntent(aFile))) {
+      if (!strcmp(ent->mnt_fsname,MOUNT_DEV)) result=strdup(ent->mnt_fsname);
+  }
+  endmntent(aFile);
+  return result;
+}
 
 /*
  * to register gdm sessions don't forget to include /etc/gdm/PreSession/Default
@@ -15,7 +32,7 @@
  */
 
 /* from https://stackoverflow.com/questions/31472040/program-to-display-all-logged-in-users-via-c-program-on-ubuntu */
-char * who() {
+char * getUsers() {
     struct utmpx *n;
     setutxent();
     n=getutxent();
@@ -52,18 +69,18 @@ int main(int argc, char *argv[]) {
     // open socket
     int sock;
     if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-        printf("could not create socket\n");
+        perror("could not create socket");
         return 1;
     }
+    char *binario=getServer();
 
     // data that will be sent to the server
     char* data_to_send = calloc(BUFFER_LENGTH,sizeof(char));
     char hostname[100];
     gethostname(hostname,100);
-    snprintf(data_to_send,BUFFER_LENGTH-1,"%s:binario1:%s",hostname,who());
+    snprintf(data_to_send,BUFFER_LENGTH-1,"%s:%s:%s",hostname,binario,getUsers());
     // send data
-    int len =
-            sendto(sock, data_to_send, strlen(data_to_send), 0,
+    int len = sendto(sock, data_to_send, strlen(data_to_send), 0,
                    (struct sockaddr*)&server_address, sizeof(server_address));
 
     // received echoed data back
